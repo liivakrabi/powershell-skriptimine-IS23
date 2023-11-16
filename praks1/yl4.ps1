17b65177037166a53b77574a7977840fd5527039
Priit Ojaots <priit.ojaots@voco.ee>
2023-11-16 11:42:15 +0200

    poolik ülesannde4

---
+++ praks1/yl4.ps1
@@ -0,0 +1,17 @@
+﻿#i
+$esimene=Read-Host "Sisesta esimene number: "
+$teine=Read-Host "Sisesta teine number: "
+
+if ($esimene -eq $teine){
+Write-Host "Need numbrid on võrdsed!"}
+
+elseif ($esimene -gt $teine){
+Write-Host "Suurem number on: $esimene"}
+
+elseif ($teine -gt $esimene){
+Write-Host "Suurem number on: $teine"}
+
+else{
+Write-Host "Midagi läks valesti"}
+
+#ii
