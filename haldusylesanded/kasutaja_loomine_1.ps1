029cabb73cc406bae15da88429755ddd96b5b61c
Priit Ojaots <priit.ojaots@voco.ee>
2023-11-23 11:38:27 +0200

    kasutaja loomine csv-iga

--- haldusylesanded/kasutaja_loomine_1.ps1
+++ haldusylesanded/kasutaja_loomine_1.ps1
@@ -1,11 +1,14 @@
-﻿if($args.Count -ne 3){
-    echo ""
-    echo "Sisestage järgmised parameetrid tühikutega: Kasutajanimi Ees_Perenimi Konto_kirjeldus"
+﻿# faili asukoht
+$Fail = "C:\Users\powerojaots\Desktop\kasutajad.csv"
+
+#loeme faili sisu
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+
+foreach ($kasutaja in $Kasutajad){
+    $KasutajaNimi = $kasutaja.Kasutajanimi
+    $TaisNimi = $kasutaja.Taisnimi
+    $KontoKirjeldus = $kasutaja.KontoKirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    New-LocalUser -Name $KasutajaNimi -Password $Parool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
+
 }
-else {
-    $KasutajaNimi = $args[0]
-    $TaisNimi = $args[1]
-    $KontoKirjeldus = $args[2]
-    $KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
-    New-LocalUser "$KasutajaNimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
-    }
