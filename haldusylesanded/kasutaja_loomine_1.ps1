d5f2f362c596f4ccfe4b8498d0a62ac775f4821f
Priit Ojaots <priit.ojaots@voco.ee>
2023-11-23 11:29:19 +0200

    kontoloomine parameetritega

---
+++ haldusylesanded/kasutaja_loomine_1.ps1
@@ -0,0 +1,11 @@
+﻿if($args.Count -ne 3){
+    echo ""
+    echo "Sisestage järgmised parameetrid tühikutega: Kasutajanimi Ees_Perenimi Konto_kirjeldus"
+}
+else {
+    $KasutajaNimi = $args[0]
+    $TaisNimi = $args[1]
+    $KontoKirjeldus = $args[2]
+    $KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+    New-LocalUser "$KasutajaNimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
+    }
