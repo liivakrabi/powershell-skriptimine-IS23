e58cbf7f1afdb8a41538e02d6f768aca75208356
Priit Ojaots <priit.ojaots@voco.ee>
2023-12-04 11:16:12 +0200

    service count

---
+++ praks1/yl12.ps1
@@ -0,0 +1,34 @@
+﻿First question:
+
+function countstart
+{
+$start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Kogu arv teenuseid töötavas seisundis = "$start.count
+}
+function countstop
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Kogu arv teenuseid peatatud seisundis= "$stop.count
+}
+countstart
+countstop
+
+function countservice
+{
+param
+(
+[string]$status
+)
+if($status -eq "Running")
+{
+$start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Kogu arv teenuseid töötavas seisundis = "$start.count
+}
+elseif($status -eq "Stopped")
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Kogu arv teenuseid peatatud seisundis= "$stop.count
+}
+}
+countservice -status Running
+countservice -status Stopped
