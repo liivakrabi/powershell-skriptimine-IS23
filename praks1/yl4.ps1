48f93786358e958aa3e24be1e83a80d6c5ce0c76
Priit Ojaots <priit.ojaots@voco.ee>
2023-12-04 10:40:25 +0200

    Lisatud teine osapool ylesandest

--- praks1/yl4.ps1
+++ praks1/yl4.ps1
@@ -15,3 +15,24 @@ else{
 Write-Host "Midagi läks valesti"}
 
 #ii
+
+Write-Host "Vali Riik" -ForegroundColor Yellow
+Write-Host "1 : India" -ForegroundColor Cyan
+Write-Host "2: Australia" -ForegroundColor Cyan
+Write-Host "3: China" -ForegroundColor Cyan
+$choice = Read-Host "Palun vali riik"
+if($choice -eq "1")
+{
+Write-Host "India pealinn on New Delhi." -ForegroundColor Green
+}
+elseif($choice -eq "2")
+{
+Write-Host "Austraalia pealinn on Canberra." -ForegroundColor Green
+}
+elseif($choice -eq "3") {
+Write-Host "Hiina pealinn on Beijing." -ForegroundColor Green
+}
+else
+{
+Write-Host "Vale valik." -ForegroundColor Red
+}
