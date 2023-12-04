fef6c8d0d88b551f266899272602b7dd4f6aca75
Priit Ojaots <priit.ojaots@voco.ee>
2023-12-04 10:50:18 +0200

    Tekstidokumendi protsesside tabamine

---
+++ praks1/yl5.ps1
@@ -0,0 +1,10 @@
+﻿Get-Process | ?{$_.ProcessName -eq "notepad"} | Select ProcessName,Id
+
+Get-ChildItem -Path "C:\temp\test"
+$file = Get-ChildItem -Path "C:\temp\test" | where {$_.Name -like "*.csv"} | Select
+Name,Length
+$sizeinKB = $file.Length/1KB
+$sizeinMB = $file.Length/1MB
+Write-Host "`nFileName : "$file.Name
+Write-Host "Size in KB : "$sizeinKB
+Write-Host "Size in MB : "$sizeinMB
