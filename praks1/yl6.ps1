d3539d69427484f79c406ba6cb68ec40aff7bf6c
Priit Ojaots <priit.ojaots@voco.ee>
2023-12-04 10:57:32 +0200

    Vanusegruppi määramine vastavalt vanusele

---
+++ praks1/yl6.ps1
@@ -0,0 +1,23 @@
+﻿$csv = Import-Csv C:\Users\powerojaots\Desktop\opilased.csv
+
+$result = @()
+foreach($c in $csv){
+    if([int]$c.Vanus -ge 4 -and [int]$c.Vanus -le 10)
+    {
+        $kool = "Junior"
+    }
+elseif([int]$c.Vanus -ge 11 -and [int]$c.Vanus -le 17)
+    {
+    $kool = "Senior"
+    }
+
+$temp = [PSCustomObject]@{
+    Nimi = $c.Name
+    Kool = $kool
+}
+
+$result += $temp
+
+}
+
+$result
