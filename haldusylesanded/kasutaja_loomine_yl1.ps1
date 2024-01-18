b95f1f1b3d23cbebbcb9e7261e601bfd3f457c5d
liivakrabi <147027122+liivakrabi@users.noreply.github.com>
2024-01-18 10:48:21 +0200

    Add files via upload

---
+++ haldusylesanded/kasutaja_loomine_yl1.ps1
@@ -0,0 +1,22 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesinimi: "
+$perenimi = Read-Host "Sisesta oma perenimi: "
+
+$eesnimi = $eesnimi.ToLower()
+$perenimi = $perenimi.ToLower()
+
+$taisnimi = "$eesnimi" + " " + "$perenimi"
+
+$kasutajanimi = "$eesnimi.$perenimi"
+
+$Parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+
+try
+{
+    New-LocalUser -Name $kasutajanimi -Password $Parool -FullName $taisnimi -ErrorAction Stop | Out-Null
+    Write-Host "Loodav kasutaja on $kasutajanimi"
+}
+catch
+{
+    Write-Host "Kasutaja loomisega tekkis probleem"
+    Write-Host "Probleemi detailid: $_"
+}
