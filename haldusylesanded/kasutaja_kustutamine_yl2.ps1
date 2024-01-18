5172258bfce6abc6cb25ccb3608d30b98df4b47a
liivakrabi <147027122+liivakrabi@users.noreply.github.com>
2024-01-18 11:00:34 +0200

    Add files via upload

---
+++ haldusylesanded/kasutaja_kustutamine_yl2.ps1
@@ -0,0 +1,18 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesinimi: "
+$perenimi = Read-Host "Sisesta oma perenimi: "
+
+$eesnimi = $eesnimi.ToLower()
+$perenimi = $perenimi.ToLower()
+
+$kasutajanimi = "$eesnimi.$perenimi"
+
+try
+{
+    Remove-LocalUser -Name $kasutajanimi
+    Write-Host "Kustutatav kasutaja on $kasutajanimi"
+}
+catch
+{
+    Write-Host "Kasutaja kustutamisega tekkis probleem"
+    Write-Host "Probleemi detailid: $_"
+}
